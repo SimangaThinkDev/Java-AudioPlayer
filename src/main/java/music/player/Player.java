925030a4330104f20df36c7cc1721babdd04f0c5
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-04-08 15:42:42 +0200

    Adding current state, (Does not have all the commands

---
+++ src/main/java/music/player/Player.java
@@ -0,0 +1,63 @@
+package music.player;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Player {
+
+    public static void main(String[] args) {
+        
+        String filePath = "/home/innocent/Documents/JAVA/Batch 4/Tuesday 8/04. Russell Zuma & George Lesley - Amanxeba (feat. Sino Msolo, Tumi Musiq & Omit ST).wav";  
+        File file = new File(filePath);
+
+        try ( AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+              Scanner scanner = new Scanner(System.in) ) {
+            
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            clip.start();
+
+            String response = "";
+
+            while ( ! response.equals("Q") ) {
+
+                System.out.print( """
+                        P -> Play
+                        S -> Stop
+                        R -> Reset
+                        Q -> Quit
+                        >>>""" 
+                        );
+
+                response = scanner.nextLine().toUpperCase();
+
+            }
+
+        } catch ( LineUnavailableException e ) {
+
+            System.out.println( "Unable to access audio resource" );
+
+        } catch ( UnsupportedAudioFileException e ) {
+
+            System.out.println( "Audio Not supported" );
+
+        } catch ( FileNotFoundException e ) {
+
+            System.out.println( "File Not Found" );
+
+        } catch ( IOException e ) {
+            System.out.println( "Something went wrong!" );
+        }
+
+    }
+
+}
